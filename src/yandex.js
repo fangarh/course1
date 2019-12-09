5127c4c147d2023d139339d4e06596add409e37e
Дворников Дмитрий Сергеевич <dds@lgt.ru>
2019-12-09 15:00:08 +0300

    course Project thread

---
+++ src/yandex.js
@@ -0,0 +1,9 @@
+const homeworkContainer = document.querySelector('#yandex-container');
+
+const mainDiv = homeworkContainer.querySelector('#mainDiv');
+
+document.addEventListener('DOMContentLoaded', onPageLoad);
+
+function onPageLoad(){
+    console.log("Started!!!");
+}
